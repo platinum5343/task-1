d641d670c252d002373fe6ff52218e9b2f1c2ef4
platinum5343 <olatunjiolamiji06@gmail.com>
2022-10-24 17:16:58 +1400

    image was added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,12 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
+import profileImg from '../../src/assets/profile__img.png'
 
-export const home = () => {
+export const Home = () => {
   return (
     <Fragment>
-        
+        <img src={profileImg} alt="profile_img" />
     </Fragment>
   )
 }
+
+export default Home;
