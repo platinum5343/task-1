5ddefd1b64b9524aa0708920b12289c92ee885c4
platinum5343 <olatunjiolamiji06@gmail.com>
2022-11-02 01:57:34 +1400

    i added media query

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,34 +1,35 @@
-import React, { Fragment } from 'react';
+import React from 'react'
 import profileImg from '../../src/assets/profile__img.png'
 import iconImg from '../../src/assets/Icon(1).png'
 import iconsImg from '../../src/assets/Icon2.png'
 import classes from './Home.module.css';
+import { Box, Typography } from '@mui/material';
 
 
-export const Home = () => {
+const Home = () => {
   return (
-    <Fragment>
-      <div  spacing={3}>
-        <div id={classes.profile_img}>
+    <Box>
+      <Box  spacing={3}>
+        <div className={classes.profile_img}>
         <img 
-        src={profileImg} id={classes.profile_img} alt="profile_img" />
+        src={profileImg} className={classes.profile_img} alt="profile_img" />
         </div>
         <div className={classes.icon} px={5} gap={"20px"}>
           <img 
           sx={{ display: {xs:"none", sm:"block"}}}
           src={iconImg} alt="icon"/>
           </div>
-        <div className={classes.icon} px={5} gap={"20px"}>
+        <Typography className={classes.icons} px={5} gap={"20px"}>
           <img 
           sx={{ display: {xs:"block", sm:"small"}}}
           src={iconsImg} alt="icon"/>
-          </div>
+          </Typography>
        
     
-        </div>
-        <div id={classes.twitter}>olatunji olamiji</div>
-    </Fragment>
+        </Box>
+        <div className={classes.twitter}>olatunji olamiji</div>
+    </Box>
   )
-}
+};
 
 export default Home;
